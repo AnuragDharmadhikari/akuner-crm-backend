ce466d8e05c155c094cf71020591951482622ff8
Anurag <dharmadhikarianurag61@gmail.com>
2026-05-23 08:49:45 +0530

    fix: exclude FlywayConfig from test profile using @Profile

--- src/main/java/org/ved/crm/config/FlywayConfig.java
+++ src/main/java/org/ved/crm/config/FlywayConfig.java
@@ -1,14 +1,14 @@
 package org.ved.crm.config;
 
 import org.flywaydb.core.Flyway;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
 
 @Configuration
-@ConditionalOnProperty(name = "spring.flyway.enabled", havingValue = "true", matchIfMissing = true)
+@Profile("!test")
 public class FlywayConfig {
 
     @Bean(initMethod = "migrate")
